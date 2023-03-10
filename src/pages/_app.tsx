6fa99e32aa98d78b9f034c5065dcfc821a21145b
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-03-10 17:53:38 -0300

    add eslint and tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tempcast</title>
+      </Head>
+
+      <Component {...pageProps} />
+
+      <style global jsx>{`
+        font-family: ${inter.style.fontFamily};
+      `}</style>
+    </>
+  )
 }
